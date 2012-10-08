7c7759009f4ad11a41b1b31e2194f0e8cfba70ec
Anisimov Denis <anisimovdenis2009@gmail.com>
2012-10-08 17:07:52 +0400

    kilui

---
+++ src/com/app/ant/app/AB/B.java
@@ -0,0 +1,11 @@
+package com.app.ant.app.AB;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: anisimov.da
+ * Date: 08.10.12
+ * Time: 17:07
+ * To change this template use File | Settings | File Templates.
+ */
+public class B {
+}

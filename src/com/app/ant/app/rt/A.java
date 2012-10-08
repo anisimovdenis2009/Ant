ca8c6046328d321f152aa8b4406e39d9b679e3f6
Anisimov Denis <anisimovdenis2009@gmail.com>
2012-10-08 17:10:14 +0400

    kilui

---
+++ src/com/app/ant/app/rt/A.java
@@ -0,0 +1,11 @@
+package com.app.ant.app.rt;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: anisimov.da
+ * Date: 08.10.12
+ * Time: 17:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class A {
+}
